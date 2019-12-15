3381d7625a45d6fdc2c15d093b5a631b58a1682d
pulkitgulati <pulkit.gemini@gmail.com>
2019-12-15 21:01:21 +0530

    radar-chart-example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import MyComp from "./MyComp";
+import MyRadarComp from "./MyRadarComp";
 
 export default class App extends Component {
     render() {
         return (
           <div className="App">
-            <MyComp />
+            {/* <MyComp /> */}
+            <MyRadarComp />
           </div>
         );
       }
